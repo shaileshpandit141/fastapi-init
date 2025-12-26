1d3e1cea21d0e1b1fe9451f5a67e187ed1158371
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-12-26 10:23:51 +0530

    feat: define core config

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    env: str = "dev"  # dev | prod
+
+    app_name: str = "FastAPI Init"
+    app_host: list[str] = ["127.0.0.1", "localhost"]
+    app_port: int = 8000
+
+    # DBs
+    sqlite_db: str = "sqlite+aiosqlite:///./dev.db"
+    postgres_db: str | None = None
+
+    @property
+    def database_url(self) -> str:
+        if self.env == "prod" and self.postgres_db:
+            return self.postgres_db
+        return self.sqlite_db
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
