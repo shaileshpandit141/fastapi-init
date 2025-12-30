8434127002f510dee38bf13d10620ade19b82c4d
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-12-30 12:23:02 +0530

    feat: update to optionally add a timedelta

--- utils/get_utc_now.py
+++ utils/get_utc_now.py
@@ -1,5 +1,10 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 
-def get_utc_now() -> datetime:
+def get_utc_now(add: timedelta | None = None) -> datetime:
+    """Get the current UTC datetime, optionally adding a timedelta."""
+
+    if add:
+        return datetime.now(timezone.utc) + add
+
     return datetime.now(timezone.utc)
