4c6a08fb97e1c81d044c5a0c6969d048245c590e
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-10 19:38:41 +0530

    feat: add create generic method

--- services/base/service.py
+++ services/base/service.py
@@ -1,7 +1,17 @@
+# pyright: reportCallIssue=false
+
+from logging import getLogger
+from typing import Any
+
 from sqlalchemy import Select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeMeta
 from sqlmodel import SQLModel, select
+
+from .exceptions import ConflictError
+
+logger = getLogger(__name__)
 
 
 class AsyncSessionService:
@@ -18,3 +28,30 @@ class AsyncCRUDService[
 
     def base_query(self) -> Select[tuple[Model]]:
         return select(self.model)
+
+    async def create(
+        self,
+        *,
+        data: CreateModel,
+        refresh: bool = True,
+        include: set[str] | None = None,
+        exclude: set[str] | None = None,
+        extra_fields: dict[str, Any] | None = None,
+    ) -> Model:
+        try:
+            obj = self.model(
+                **data.model_dump(include=include, exclude=exclude),
+                **(extra_fields or {}),
+            )
+            self.session.add(obj)
+            await self.session.commit()
+            if refresh:
+                await self.session.refresh(obj)
+            return obj
+        except IntegrityError as error:
+            logger.debug(
+                f"{self.__class__.__name__} record creation failed: ",
+                exc_info=error,
+            )
+            await self.session.rollback()
+            raise ConflictError from error
