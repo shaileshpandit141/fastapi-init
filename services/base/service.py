067fe6734c2da8e07f25fd8c0a44f50dc35b4e51
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-10 19:15:22 +0530

    feat: add generic async crud service

--- services/base/service.py
+++ services/base/service.py
@@ -1,6 +1,20 @@
-from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy import Select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeMeta
+from sqlmodel import SQLModel, select
 
 
 class AsyncSessionService:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
+
+
+class AsyncCRUDService[
+    Model: DeclarativeMeta, CreateModel: SQLModel, UpdateModel: SQLModel
+]:
+    def __init__(self, *, session: AsyncSession, model: type[Model]) -> None:
+        self.session = session
+        self.model = model
+
+    def base_query(self) -> Select[tuple[Model]]:
+        return select(self.model)
