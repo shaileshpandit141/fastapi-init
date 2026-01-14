88f61e108ec3daeb4351635b5b057fa54918ef6e
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-14 19:15:28 +0530

    feat: add otp generator class

---
+++ core/security/otp/generator.py
@@ -0,0 +1,10 @@
+from secrets import randbelow
+
+
+class OtpGenerator:
+    @staticmethod
+    def generate(length: int = 6) -> str:
+        if length < 1:
+            raise ValueError("OTP length must be at least 1")
+
+        return "".join(str(randbelow(10)) for _ in range(length))
