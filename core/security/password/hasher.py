23344bda2d0ffd908da6c5be2cfa67c7da6f841b
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-14 19:31:35 +0530

    feat: add create user in user service

--- core/security/password/hasher.py
+++ core/security/password/hasher.py
@@ -9,11 +9,11 @@ class PasswordHasher:
         deprecated="auto",
     )
 
-    def hash_password(self, *, password: str | bytes) -> str:
+    def hash_password(self, password: str | bytes) -> str:
         return self._context.hash(password)
 
     def verify_password(
-        self, *, plain_password: str | bytes, hashed_password: str | bytes
+        self, plain_password: str | bytes, hashed_password: str | bytes
     ) -> bool:
         return self._context.verify(
             plain_password,
@@ -21,7 +21,7 @@ class PasswordHasher:
         )
 
     def verify_and_update(
-        self, *, plain_password: str | bytes, hashed_password: str
+        self, plain_password: str | bytes, hashed_password: str
     ) -> tuple[bool, str | None]:
         verified, new_hash = self._context.verify_and_update(
             plain_password,
---
+++ domain/user/service.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException, status
+
+from core.repository.exceptions import ConflictError
+from core.security.password.hasher import PasswordHasher
+from domain.user.models import User, UserStatus
+from domain.user.repository import UserRepository
+from domain.user.schemas import UserCreate
+
+
+class UserService:
+    def __init__(self, user_repo: UserRepository) -> None:
+        self.user_repo = user_repo
+
+    async def create_user(self, user_in: UserCreate) -> User:
+
+        hasher = PasswordHasher()
+        password_hash = hasher.hash_password(user_in.password)
+
+        try:
+            user = await self.user_repo.create(
+                data=user_in,
+                include={"email"},
+                extra={
+                    "password_hash": password_hash,
+                    "status": UserStatus.ACTIVE,
+                },
+            )
+        except ConflictError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered",
+            )
+
+        return user
