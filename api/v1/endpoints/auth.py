7ad00cb7e5325a44c109d23fdaff78fea1acd74b
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-14 20:28:10 +0530

    feat: update create user endpoint to user service

--- api/v1/endpoints/auth.py
+++ api/v1/endpoints/auth.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from domain.auth.deps import AuthServiceDep, OAuth2PasswordRequestFormDep
 from domain.auth.schemas import JwtTokenCreate, TokenRead, TokenRefresh, TokenRevoked
 from domain.response.schemas import DetailResponse
-from domain.user.deps import ActiveUserDep
+from domain.user.deps import ActiveUserDep, UserServiceDep
 from domain.user.models import User
 from domain.user.schemas import UserCreate, UserRead
 
@@ -11,13 +11,13 @@ router = APIRouter(prefix="/auth", tags=["Auth Endpoints"])
 
 
 @router.post(
-    "/signup",
-    summary="Create new user",
-    description="Create a new user with email and password.",
+    "/register",
+    summary="Register a new user",
+    description="Register a new user with email and password.",
     response_model=UserRead,
 )
-async def create_user(user_in: UserCreate, auth_service: AuthServiceDep) -> User:
-    return await auth_service.register(user_in=user_in)
+async def create_user(user_in: UserCreate, user_service: UserServiceDep) -> User:
+    return await user_service.create_user(user_in)
 
 
 @router.post(
