bf8fe18837e2cfc2cfcaf66b9cd81e30dbcdd636
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-15 19:25:05 +0530

    feat: add get role method in role service

--- domain/rbac/service.py
+++ domain/rbac/service.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, status
 
-from core.repository.exceptions import ConflictError
+from core.repository.exceptions import ConflictError, NotFoundError
 
 from .models import Role
 from .repository import RoleRepository
@@ -19,6 +19,16 @@ class RoleService:
         except ConflictError:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="This role is exist"
+            )
+
+        return role
+
+    async def get_role(self, role_id: int) -> Role:
+        try:
+            role = await self.role_repo.get_or_raise(id=role_id)
+        except NotFoundError:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Role does not exist"
             )
 
         return role
