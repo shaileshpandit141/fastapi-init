cfd0efba384d96b52e6b854474f16e50423acd81
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-15 21:44:16 +0530

    feat: add get role permission service dep

--- domain/rbac/deps.py
+++ domain/rbac/deps.py
@@ -4,9 +4,9 @@ from fastapi import Depends
 
 from core.db.deps import AsyncSessionDep
 
-from .models import Permission, Role
-from .repository import PermissionRepository, RoleRepository
-from .service import PermissionService, RoleService
+from .models import Permission, Role, RolePermission
+from .repository import PermissionRepository, RolePermissionRepository, RoleRepository
+from .service import PermissionService, RolePermissionService, RoleService
 
 
 async def get_role_service(session: AsyncSessionDep) -> RoleService:
@@ -17,5 +17,16 @@ async def get_permission_service(session: AsyncSessionDep) -> PermissionService:
     return PermissionService(PermissionRepository(model=Permission, session=session))
 
 
+async def get_role_permission_service(
+    session: AsyncSessionDep,
+) -> RolePermissionService:
+    return RolePermissionService(
+        RolePermissionRepository(model=RolePermission, session=session)
+    )
+
+
 RoleServiceDep = Annotated[RoleService, Depends(get_role_service)]
 PermissionServiceDep = Annotated[PermissionService, Depends(get_permission_service)]
+RolePermissionServiceDep = Annotated[
+    RolePermissionService, Depends(get_role_permission_service)
+]
