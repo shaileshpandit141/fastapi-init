1891cea3962f81f6db86766b419a09baf9d82d25
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-18 18:30:02 +0530

    refactor: add init db file

---
+++ core/db/startup/init_db.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel
+
+from ..engines.async_engine import async_engine
+
+
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+__all__ = ["init_db"]
