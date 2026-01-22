c45def915cda05cfca28ffbef320ec04c2f356ca
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-22 15:35:19 +0530

    refactor: remove model args in service

--- domain/rbac/depends/user_role.py
+++ domain/rbac/depends/user_role.py
@@ -4,14 +4,13 @@ from fastapi import Depends
 
 from core.db.depends.async_session import AsyncSessionDep
 
-from ..models.user_role import UserRole
 from ..services.user_role import UserRoleService
 
 # === User Role Service Dep ===
 
 
 async def get_user_role_service(session: AsyncSessionDep) -> UserRoleService:
-    return UserRoleService(model=UserRole, session=session)
+    return UserRoleService(session=session)
 
 
 UserRoleServiceDep = Annotated[UserRoleService, Depends(get_user_role_service)]
