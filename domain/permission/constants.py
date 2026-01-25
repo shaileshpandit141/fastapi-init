65817623cadd22393ecdb0b806a5e992f778d1cd
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-25 11:03:11 +0530

    feat: add more perm constants

--- domain/permission/constants.py
+++ domain/permission/constants.py
@@ -1,16 +1,39 @@
 from enum import StrEnum
 
 
-class Perm:
-    class User(StrEnum):
-        FULL = "user:*"
-        CREATE = "user:create"
-        READ = "user:read"
-        UPDATE = "user:update"
-        DELETE = "user:delete"
+class UserPerm(StrEnum):
+    FULL = "user:*"
+    CREATE = "user:create"
+    READ = "user:read"
+    UPDATE = "user:update"
+    DELETE = "user:delete"
 
-    class UserRole(StrEnum):
-        FULL = "user:role:*"
-        ASSIGN = "user:role:assign"
-        REVOKE = "user:role:revoke"
-        LIST = "user:role:list"
+
+class UserRolePerm(StrEnum):
+    FULL = "user:role:*"
+    ASSIGN = "user:role:assign"
+    REVOKE = "user:role:revoke"
+    LIST = "user:role:list"
+
+
+class RolePerm(StrEnum):
+    FULL = "role:*"
+    CREATE = "role:create"
+    READ = "role:read"
+    UPDATE = "role:update"
+    DELETE = "role:delete"
+
+
+class PermissionPerm(StrEnum):
+    FULL = "permission:*"
+    CREATE = "permission:create"
+    READ = "permission:read"
+    UPDATE = "permission:update"
+    DELETE = "permission:delete"
+
+
+class RolePermissionPerm(StrEnum):
+    FULL = "role:permission:*"
+    ASSIGN = "role:permission:assign"
+    REVOKE = "role:permission:revoke"
+    LIST = "role:permission:list"
