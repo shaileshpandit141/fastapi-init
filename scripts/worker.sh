0559662335f4dcee5b30d19a162a7729f324d61a
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-01-28 18:49:11 +0530

    feat: update worker script

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
-set -e
 
-export $(grep -v '^#' .env | xargs)
-
-CELERY_APP="celery_app:celery"
-LOG_LEVEL="DEBUG"
-QUEUE=${CELERY_QUEUE:-default}
-
-echo "Starting Celery Worker"
-echo "Queue: $QUEUE"
-
-exec uv run celery -A "$CELERY_APP" worker \
-  --loglevel="$LOG_LEVEL" \
-  --queues="$QUEUE" \
-  --concurrency=1 \
-  --pool=solo
+exec uv run celery -A celery_app:celery worker -l info
