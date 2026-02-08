992a15df94a5f5ec5426fd57ea47724f151d6edd
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-02-08 20:53:17 +0530

    feat: add notification get functionality

--- domain/notification/services.py
+++ domain/notification/services.py
@@ -3,7 +3,7 @@ from uuid import UUID
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from core.exceptions.http_exception import AlreadyExistsError
+from core.exceptions.http_exception import AlreadyExistsError, NotFoundError
 from core.repositories.exceptions import EntityConflictError
 
 from .models import Notification
@@ -19,10 +19,10 @@ class NotificationService:
     def __init__(self, session: AsyncSession) -> None:
         self.repo = NotificationRepository(model=Notification, session=session)
 
-    async def create(self, data: NotificationCreate) -> Notification:
+    async def create(self, data: NotificationCreate, user_id: int) -> Notification:
         try:
             notification = await self.repo.create(
-                data=data,
+                data=data, values={"user_id": user_id}
             )
         except EntityConflictError:
             raise AlreadyExistsError("Notification already exists.")
@@ -32,22 +32,27 @@ class NotificationService:
     async def list(
         self, user_id: int, limit: int = 10, offset: int = 0
     ) -> Sequence[Notification]:
-        permissions = await self.repo.find_by(
+        notifications = await self.repo.find_by(
             conditions=[Notification.user_id == user_id]
         )
 
-        return permissions
+        return notifications
 
-    async def get(self, notification_id: UUID) -> Notification:
-        raise NotImplementedError
+    async def get(self, notification_id: UUID, user_id: int) -> Notification:
+        notification = await self.repo.get_by(id=notification_id, user_id=user_id)
 
-    async def mark_as_read(self, user_id: int, notification_id: UUID) -> Notification:
+        if not notification:
+            raise NotFoundError(detail="Notification not found.")
+
+        return notification
+
+    async def mark_as_read(self, notification_id: UUID, user_id: int) -> Notification:
         raise NotImplementedError
 
     async def mark_all_as_read(
-        self, user_id: int, notification_id: UUID
+        self, notification_id: UUID, user_id: int
     ) -> Sequence[Notification]:
         raise NotImplementedError
 
-    async def delete_read(self, user_id: int, notification_id: UUID) -> Notification:
+    async def delete_read(self, notification_id: UUID, user_id: int) -> Notification:
         raise NotImplementedError
