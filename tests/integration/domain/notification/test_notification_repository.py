3a3accc34da5eccb5c8447240b9019f4265447b4
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-02-11 17:25:13 +0530

    test: write create notifications test case

---
+++ tests/integration/domain/notification/test_notification_repository.py
@@ -0,0 +1,34 @@
+import pytest
+
+from core.repository.actions.insert import AsyncSession
+from domain.notification.constants import NotificationEvent
+from domain.notification.repositories import NotificationRepository
+from domain.notification.schemas import NotificationCreate
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_create_notifications_repository(
+    async_session: AsyncSession,
+) -> None:
+    repo = NotificationRepository(async_session)
+    results = await repo.create_notifications(
+        user_id=1,
+        data=[
+            NotificationCreate(
+                title="Welcome to the platform",
+                message="Your account has been successfully created.",
+                event=NotificationEvent.SYSTEM,
+            ),
+            NotificationCreate(
+                title="Password Reset Requested",
+                message="Click the link to reset your password.",
+                event=NotificationEvent.COMMENT,
+            ),
+        ],
+    )
+    await async_session.commit()
+
+    # Assertions
+    assert len(results) > 0
+    assert results[0].user_id == 1
