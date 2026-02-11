c1401caff30208251775bc0eea71e43b37033f48
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-02-11 19:27:40 +0530

    feat: add update one repo action

--- core/repository/actions/update.py
+++ core/repository/actions/update.py
@@ -7,9 +7,33 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ..base import BaseAction
 
+# =============================================================================
+# Update One Record
+# =============================================================================
+
+
+class UpdateOne[T: SQLModel](BaseAction[T]):
+    def __init__(self, obj: T, data: BaseModel | SQLModel) -> None:
+        self.obj = obj
+        self.data = data
+
+    async def execute(self, session: AsyncSession) -> T:
+        values = self.data.model_dump(exclude_unset=True)
+
+        for k, v in values.items():
+            setattr(self.obj, k, v)
+
+        session.add(self.obj)
+        return self.obj
+
+
+# =============================================================================
+# Update Many Records
+# =============================================================================
+
 
 class UpdateMany[T: SQLModel](BaseAction[Sequence[T]]):
-    def __init__(self, objs: Sequence[T], data: BaseModel) -> None:
+    def __init__(self, objs: Sequence[T], data: BaseModel | SQLModel) -> None:
         self.objs = objs
         self.data = data
 
