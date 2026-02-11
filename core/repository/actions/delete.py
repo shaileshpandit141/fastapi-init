1ec2d9cefefc0b29a2500743c2ee6d967e0c6a51
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-02-11 19:13:50 +0530

    feat: add delete one repo action

--- core/repository/actions/delete.py
+++ core/repository/actions/delete.py
@@ -5,6 +5,24 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ..base import BaseAction
 
+# =============================================================================
+# Delete One Records
+# =============================================================================
+
+
+class DeleteOne[T: SQLModel](BaseAction[int]):
+    def __init__(self, obj: T) -> None:
+        self.obj = obj
+
+    async def execute(self, session: AsyncSession) -> int:
+        await session.delete(self.obj)
+        return 1
+
+
+# =============================================================================
+# Delete Many Records
+# =============================================================================
+
 
 class DeleteMany[T: SQLModel](BaseAction[int]):
     def __init__(self, objs: Sequence[T]) -> None:
