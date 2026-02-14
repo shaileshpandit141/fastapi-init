0e45ea3064b1bca76c63ef4e4504951a2c6161bb
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-02-14 14:39:39 +0530

    feat: add global app error

---
+++ app/shared/exceptions.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+# =============================================================================
+# Root app error (used globaly)
+# =============================================================================
+
+
+class AppError(Exception):
+    def __init__(self, detail: str | list[Any]) -> None:
+        super().__init__(detail)
+        self.detail = detail
