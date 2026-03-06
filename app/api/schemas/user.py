a2b97b3f53938559246dd77e448bbbda48ed03c1
shaileshpandit141 <shaileshpandit141@gmail.com>
2026-03-06 14:34:27 +0530

    refactor: update user read schema

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -2,5 +2,5 @@ from app.adapters.db.models._mixins import UUIDv7Mixin, TimestampMixin
 from app.adapters.db.models.user import UserBase
 
 
-class UserRead(TimestampMixin, UUIDv7Mixin, UserBase):
+class UserRead(TimestampMixin, UserBase, UUIDv7Mixin):
     pass
